e176b197a26410e36da8b4f439c56cc552a40ce9
Fernando Tello Leon <fernando_tellol@hotmail.com>
2024-08-23 18:53:39 -0500

    agregado boton crear post en menu principal

--- src/components/header/MainMenuHeader.jsx
+++ src/components/header/MainMenuHeader.jsx
@@ -3,37 +3,37 @@ import { Link } from 'react-router-dom'
 export const MainMenuHeader = () => {
   return (
     <>
-      <li className='list-none mb-8 sm:mb-0 md:cursor-pointer border-b border-b-gray-500 w-[60%] text-center leading-loose md:leading-none md:text-start md:w-fit md:mr-2 md:border-none'>
+      <li className='list-none mb-8 sm:mb-0 md:cursor-pointer border-b border-b-gray-500 text-center leading-loose md:leading-none md:text-start md:w-fit md:mr-2 md:border-none'>
         <Link
           to='/'
-          className='text-2xl font-extrabold uppercase hover:text-secondary text-4 p-3 md:hover:text-secondary md:hover:border-b md:hover:border-secondary transition-all md:text-base'
+          className='text-2xl font-extrabold uppercase hover:text-secondary p-3 md:hover:text-secondary md:hover:border-b md:hover:border-secondary transition-all md:text-base'
         >
           Inicio
         </Link>
       </li>
 
-      <li className='list-none mb-8 sm:mb-0 md:cursor-pointer border-b border-b-gray-500 w-[60%] text-center leading-loose md:leading-none md:text-start md:w-fit md:mr-2 md:border-none'>
+      <li className='list-none mb-8 sm:mb-0 md:cursor-pointer border-b border-b-gray-500 text-center leading-loose md:leading-none md:text-start md:w-fit md:mr-2 md:border-none'>
         <Link
           to='/categories'
-          className='text-2xl font-extrabold uppercase hover:text-secondary text-4 p-3 md:hover:text-secondary md:hover:border-b md:hover:border-secondary transition-all md:text-base'
+          className='text-2xl font-extrabold uppercase hover:text-secondary p-3 md:hover:text-secondary md:hover:border-b md:hover:border-secondary transition-all md:text-base'
         >
           Categorias
         </Link>
       </li>
 
-      <li className='list-none mb-8 sm:mb-0 md:cursor-pointer border-b border-b-gray-500 w-[60%] text-center leading-loose md:leading-none md:text-start md:w-fit md:mr-2 md:border-none'>
+      <li className='list-none mb-8 sm:mb-0 md:cursor-pointer border-b border-b-gray-500 text-center leading-loose md:leading-none md:text-start md:w-fit md:mr-2 md:border-none'>
         <Link
           to='/about'
-          className='text-2xl font-extrabold uppercase hover:text-secondary text-4 p-3 md:hover:text-secondary md:hover:border-b md:hover:border-secondary transition-all md:text-base'
+          className='text-2xl font-extrabold uppercase hover:text-secondary p-3 md:hover:text-secondary md:hover:border-b md:hover:border-secondary transition-all md:text-base'
         >
           Nosotros
         </Link>
       </li>
 
-      <li className='list-none mb-8 sm:mb-0 md:cursor-pointer border-b border-b-gray-500 w-[60%] text-center leading-loose md:leading-none md:text-start md:w-fit md:mr-2 md:border-none'>
+      <li className='list-none mb-8 sm:mb-0 md:cursor-pointer border-b border-b-gray-500 text-center leading-loose md:leading-none md:text-start md:w-fit md:border-none md:ml-4'>
         <Link
           to='/create-post'
-          className='text-2xl font-extrabold uppercase hover:text-secondary text-4 p-3 md:hover:text-secondary md:hover:border-b md:hover:border-secondary transition-all md:text-base'
+          className='text-2xl font-extrabold uppercase hover:text-slate-900 hover:bg-orange-400 p-3 md:py-2 md:px-4 md:bg-blue-500 md:text-slate-100 transition-all md:text-xs md:rounded-xl'
         >
           Crear Post
         </Link>
