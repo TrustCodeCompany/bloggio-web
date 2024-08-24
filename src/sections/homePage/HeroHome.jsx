e5f335df56ec0c3626dbe067907c61ae78ef4b19
Fernando Tello Leon <fernando_tellol@hotmail.com>
2024-08-23 23:18:47 -0500

    agregado boton de crear post en Seccion Hero

--- src/sections/homePage/HeroHome.jsx
+++ src/sections/homePage/HeroHome.jsx
@@ -1,13 +1,27 @@
+import { Link } from 'react-router-dom'
+import { useUserStore } from '../../store/userStore'
+
 export const HeroHome = () => {
+  const { logged } = useUserStore()
+
   return (
-    <>
-      <h1 className='text-2xl md:text-3xl font-bold font-Oswald text-center mx-auto mb-3 mt-16'>
+    <div className=' mb-28'>
+      <h1 className='text-2xl md:text-3xl font-bold font-Oswald text-center mx-auto mt-28'>
         Blog con Libertad: Historias y entrevistas
       </h1>
-      <p className='text-gray-700 text-sm text-center mb-16'>
+      <p className='text-gray-700 text-sm text-center mb-4'>
         Suscribete para aprender acerca de nuevos productos y últimas
         tecnologías
       </p>
-    </>
+      <div className='flex md:justify-center md:items-center mx-auto '>
+        <p className='mr-4 font-bold w-[70%] md:w-fit'>Comienza a plasmar tus ideas en un nuevo Post</p>
+        <Link
+          to={logged ? '/create-post' : '/login-by-createPost'}
+          className=' flex justify-center items-center font-bold hover:text-slate-900 hover:bg-orange-400 py-2 px-4 bg-slate-900 text-slate-100 transition-all text-xs rounded-xl shadow-xl'
+        >
+          Crear Post
+        </Link>
+      </div>
+    </div>
   )
 }
