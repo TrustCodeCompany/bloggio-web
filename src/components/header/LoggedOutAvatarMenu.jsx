65e4172d2a9834f9c2136c6c9b574b744c6f1e0a
Fernando Tello Leon <fernando_tellol@hotmail.com>
2024-09-12 21:55:43 -0500

    refactorizado color bootn

--- src/components/header/LoggedOutAvatarMenu.jsx
+++ src/components/header/LoggedOutAvatarMenu.jsx
@@ -4,7 +4,7 @@ export const LoggedOutAvatarMenu = () => {
   return (
     <>
       <Link
-        className='bg-slate-900 text-slate-200 font-bold px-4 py-4 rounded-xl transition-all hover:text-secondary hover:shadow-xl md:ml-2 mb-6 mt-10 md:mt-0 md:mb-0 w-2/3 md:w-fit text-center uppercase text-lg md:text-xs md:py-2 md:px-8'
+        className='bg-slate-900 text-slate-200 font-bold px-4 py-4 rounded-xl transition-all dark:bg-bgDarkGrayColor dark:hover:bg-dark-secondary dark:border-gray-400 border dark:text-dark-textColor dark:hover:text-dark-textColor hover:text-secondary hover:shadow-xl md:ml-2 mb-6 mt-10 md:mt-0 md:mb-0 w-2/3 md:w-fit text-center uppercase text-lg md:text-xs md:py-2 md:px-8'
         to='/login'
       >
         Login
--- src/components/header/MainMenuHeader.jsx
+++ src/components/header/MainMenuHeader.jsx
@@ -27,7 +27,7 @@ export const MainMenuHeader = () => {
       <li className='list-none mb-8 sm:mb-0 md:cursor-pointer border-b border-b-gray-500  dark:border-b-dark-accent  text-center leading-loose md:leading-none md:text-start md:w-fit md:border-none md:ml-4'>
         <Link
           to={logged ? '/create-post' : '/login-by-createPost'}
-          className='text-2xl font-extrabold uppercase hover:text-slate-900 hover:bg-orange-400  dark:hover:text-dark-accent md:dark:hover:border-dark-accent  p-3 md:py-2 md:px-4 md:bg-blue-500 md:text-slate-100 transition-all md:text-xs md:rounded-xl'
+          className='text-2xl font-extrabold uppercase hover:text-slate-900 hover:bg-orange-400  dark:hover:text-dark-primary md:dark:hover:border-dark-accent  p-3 md:py-2 md:px-4 md:bg-blue-500 md:text-slate-100 transition-all md:text-xs md:rounded-xl'
         >
           Crear Post
         </Link>
